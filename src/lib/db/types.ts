c448a9bb8e5a5e814bf24dcff0f84564ef03aefa
Adam Seefried <aseefried@gmail.com>
2026-06-12 11:04:43 -0400

    [#125] Add getHorseIncomeSummary to lessons DA layer

--- src/lib/db/types.ts
+++ src/lib/db/types.ts
@@ -101,3 +101,9 @@ export interface HorseExertionSummary {
   lessonCount: number
   totalExertion: number
 }
+
+export interface HorseIncomeSummary {
+  horseId: string
+  horseName: string
+  totalIncome: number
+}
